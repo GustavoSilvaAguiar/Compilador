4136619cde5c5e80583140285779f984da700fc7
GustavoSilvaAguiar <gustavogo109@gmail.com>
2024-10-09 09:53:47 -0300

    first commit

---
+++ Compiler.java
@@ -0,0 +1,8 @@
+public class Compiler {
+    public static void main(String[] args) {
+        String sourceCode = "teste";
+        //implementar analisador lexico
+        //implementar o parser
+
+    }
+}
